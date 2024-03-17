d877342161c3de089ae13106ed978190156a3ec7
emre <emre>
2024-03-17 13:20:48 +0300

    Foreah

---
+++ app.js
@@ -0,0 +1,18 @@
+// Foreach döngüsü : Dizilerde kullanıyoruz
+
+let isimler =["Emre","Ali","Ayşe","Ceren","Veli","Kübra","Adem"];
+
+isimler.forEach (function (isim){
+    console.log(isim);
+}); 
+
+
+// for(i=0; i<isimler.length; i++) {
+//     console.log(isimler[i]);
+// }
+
+// let sayac = 0;
+// while(sayac<isimler.length){
+//     console.log(isimler[sayac]);
+//     sayac++;
+// }
